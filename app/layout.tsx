d840d43f786bc88674a9151f3dd36cfe130ca400
Octavio Duarte <octiduarte@hotmail.com>
2025-10-21 20:15:38 -0300

    Ahora trae los ultimos 3 reportes cargados, Tambien se realizaron leves cambios de estetica

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
-import Header from '@/components/header'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
-  title: 'JackeMate',
-  description: 'Created by Facundo y Octavio',
-  generator: 'v0.app',
-}
+  title: "JackeMate",
+  description: "Created by Facundo y Octavio",
+  generator: "v0.app",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-  <Header />
-  {children}
+        <Header />
+        {children}
       </body>
     </html>
-  )
+  );
 }
